4bc1aeea0da27f1822f2b58ac56dc98530119fdf
bassant2005 <basant.tareq.2005@gmail.com>
2025-11-28 17:04:21 +0200

    final version

--- creating_data_files.cpp
+++ creating_data_files.cpp
@@ -291,44 +291,5 @@ void Build_indexes(){
     auto doctorPrimary = buildPrimaryIndexLength(doctorDataFile, doctorPrimaryIndexFile, 3);
     auto doctorSecondary = buildSecondaryIndex(doctorDataFile,doctorDataFile, doctorSecondaryIndexFile, 1, 3);
     auto appointmentPrimary = buildPrimaryIndexLength(appointmentDataFile, appointmentPrimaryIndexFile, 1);
-    auto appointmentSecondary = buildSecondaryIndex(appointmentDataFile,
-                                                    doctorDataFile,appointmentSecondaryIndexFile, 2, 1);
+    auto appointmentSecondary = buildSecondaryIndex(appointmentDataFile,doctorDataFile,appointmentSecondaryIndexFile, 2, 1);
 }
-
-// int main() {
-//     cout << "=== Building indexes for Doctors.txt ===\n";
-//     auto doctorPrimary = buildPrimaryIndexLength(doctorDataFile, doctorPrimaryIndexFile, 3); // DoctorID
-//     auto doctorSecondary = buildSecondaryIndex(doctorDataFile,doctorDataFile, doctorSecondaryIndexFile, 1, 3); // Name -> ID
-//
-//     cout << "Doctor Primary Index (ID | Offset):\n";
-//     for (auto &d : doctorPrimary)
-//         cout << d.recordID << " | " << d.offset<< "\n";
-//
-//     cout << "\nDoctor Secondary Index (Name -> ID):\n";
-//     for (auto &d : doctorSecondary)
-//         cout << d.keyValue << " | " << d.linkedID << "\n";
-//
-//     cout << "\n=== Building indexes for Appointments.txt ===\n";
-//     auto appointmentPrimary = buildPrimaryIndexLength(appointmentDataFile, appointmentPrimaryIndexFile, 1); // AppointmentID
-//     auto appointmentSecondary = buildSecondaryIndex(appointmentDataFile, doctorDataFile, appointmentSecondaryIndexFile, 2, 1); // DoctorID -> AppointmentID
-//
-//     cout << "Appointment Primary Index (ID | Offset):\n";
-//     for (auto &a : appointmentPrimary)
-//         cout << a.recordID << " | " << a.offset << "\n";
-//
-//     cout << "\nAppointment Secondary Index (DoctorID -> AppointmentID):\n";
-//     for (auto &a : appointmentSecondary)
-//         cout << a.keyValue << " | " << a.linkedID << "\n";
-//
-//     // Test reading back from indexes files
-//     cout << "\n=== Read indexes ===\n";
-//     auto doctorPrimaryRead = readPrimaryIndex(doctorPrimaryIndexFile);
-//     cout << "\nRead back Doctor Primary Index:\n";
-//     for (auto &d : doctorPrimaryRead)
-//         cout << d.recordID << " | " << d.offset << "\n";
-//
-//     auto doctorSecondaryRead = readSecondaryIndex(doctorSecondaryIndexFile);
-//     cout << "\nRead back Doctor Secondary Index:\n";
-//     for (auto &d : doctorSecondaryRead)
-//         cout << d.keyValue << " | " << d.linkedID << "\n";
-// }
